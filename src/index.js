36b659b99591deed00e202a1a1f35eb7cfec9ea5
Ewa Zawilińska <zawilecewa@gmail.com>
2023-09-16 12:42:12 +0200

    .

---
+++ src/index.js
@@ -0,0 +1,107 @@
+import * as catApi from './js/api_pixabay';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import _ from 'lodash';
+import Waypoint from 'waypoints/lib/noframework.waypoints.min.js';
+
+const gallery = document.querySelector('.gallery');
+const searchInput = document.querySelector('.search-form input');
+const searchForm = document.querySelector('.search-form');
+const lightbox = new SimpleLightbox('.photo-card a', {
+  /* options */
+});
+
+const moreBtn = document.querySelector('.load-more');
+
+let pageImages = 1;
+let foundImagesCount = null;
+let searchQuery = null;
+let isLoading = false;
+
+async function getImages(searchQuery, page, callback) {
+  try {
+    const answer = await catApi.searchImage(searchQuery, page);
+    callback(answer);
+
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+
+    isLoading = false;
+  } catch (error) {
+    Notiflix.Notify.failure(`Błąd wczytywania spróbuj jeszcze raz.`);
+    console.log(error);
+  }
+}
+function displayImages(data) {
+  foundImagesCount = data.totalHits;
+  Notiflix.Notify.success(`Znaleziono ${foundImagesCount} zdjęć`);
+  createPostes(data.hits);
+}
+function addPosts(data) {
+  createPostes(data.hits);
+}
+function moreImages() {
+  const scrollPosition = window.pageYOffset;
+  const windowHeight = window.innerHeight;
+  const documentHeight = document.documentElement.scrollHeight;
+
+  if (!(scrollPosition + windowHeight >= documentHeight) || isLoading) return;
+
+  isLoading = true;
+  if (checkRemainingImages()) {
+    getImages(searchQuery, ++pageImages, addPosts);
+    return;
+  }
+  Notiflix.Notify.info('Wyświetlono wszystkie zdjęcia z bazy!');
+}
+function searchImages(eve) {
+  eve.preventDefault();
+  pageImages = 1;
+  gallery.innerHTML = '';
+  searchQuery = searchInput.value;
+  getImages(searchQuery, pageImages, displayImages);
+  searchInput.value = '';
+}
+function createPostes(images) {
+  const markupImages = images
+    .map(
+      image =>
+        `  
+        <div class="photo-card">
+            <a href="${image.largeImageURL}"><img src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/></a>
+            <div class="info">
+                <p class="info-item">
+                    <b>Likes </b>${image.likes}
+                </p>
+                <p class="info-item">
+                    <b>Views </b>${image.views}
+                </p>
+                <p class="info-item">
+                    <b>Comments </b>${image.comments}
+                </p>
+                <p class="info-item">
+                    <b>Downloads </b>${image.downloads}
+                </p>
+            </div>
+        </div>`
+    )
+    .join(' ');
+  gallery.innerHTML += markupImages;
+  lightbox.refresh();
+}
+function checkRemainingImages() {
+  if (pageImages <= foundImagesCount / 20) {
+    return true;
+  }
+  return false;
+}
+
+searchForm.addEventListener('submit', searchImages);
+document.addEventListener('scroll', _.throttle(moreImages, 300));
---
+++ src/js/api_pixabay.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const urlApi = `https://pixabay.com/api/`;
+const apiKey = '38025417-155cab9872a1d53dd80473204';
+
+export function init(api_key) {
+  axios.defaults.headers.common['x-api-key'] = api_key;
+}
+export async function searchImage(query, page) {
+  const url = `${urlApi}?key=${apiKey}&q=${encodeURIComponent(
+    query
+  )}&page=${page}&per_page=20&image_type=photo&orientation=horizontal&safesearch=true`;
+  const response = await axios.get(url);
+  return response.data;
+}
